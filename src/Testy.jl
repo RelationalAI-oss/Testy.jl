db951fd7e9e3133d6709b991665e6ccdee89e812
Nathan Daly <nathan.daly@relational.ai>
2019-10-17 12:46:18 -0400

    Updated Testy's regex for the changes in Julia 1.3
    This commit essentially applies the changes in
    https://github.com/JuliaLang/julia/pull/32381 to the Regex functions in
    Testy.jl

--- src/Testy.jl
+++ src/Testy.jl
@@ -6,16 +6,25 @@ using Test
 """
 Helper for `pmatch`.
 """
-function pexec(re,subject,offset,options,match_data)
+function pexec(re, subject, offset, options, match_data)
     rc = ccall((:pcre2_match_8, Base.PCRE.PCRE_LIB), Cint,
                (Ptr{Cvoid}, Ptr{UInt8}, Csize_t, Csize_t, Cuint, Ptr{Cvoid}, Ptr{Cvoid}),
-               re, subject, sizeof(subject), offset, options, match_data, Base.PCRE.MATCH_CONTEXT[])
+               re, subject, sizeof(subject), offset, options, match_data, Base.PCRE.get_local_match_context())
     # rc == -1 means no match, -2 means partial match.
     rc < -2 && error("PCRE.exec error: $(err_message(rc))")
     rc
 end
 
+# Mirrors Base.PCRE.exec_r_data: helper to call pexec w/ thread-safe match data
+function pexec_r_data(re, subject, offset, options)
+    match_data = Base.PCRE.create_match_data(re)
+    ans = pexec(re, subject, offset, options, match_data)
+    return ans, match_data
+end
+
 """
+   pmatch(r::Regex, s::AbstractString[, idx::Integer[, addopts]])
+
 Variant of `Base.match` that supports partial matches (when `Base.PCRE.PARTIAL_HARD`
 is set in `re.match_options`).
 """
@@ -23,18 +32,21 @@ function pmatch(re::Regex, str::Union{SubString{String}, String}, idx::Integer, 
     Base.compile(re)
     opts = re.match_options | add_opts
     # rc == -1 means no match, -2 means partial match.
-    rc = pexec(re.regex, str, idx-1, opts, re.match_data)
+    rc, data = pexec_r_data(re.regex, str, idx-1, opts)
     if rc == -1 || (rc == -2 && (re.match_options & Base.PCRE.PARTIAL_HARD) == 0)
+        Base.PCRE.free_match_data(data)
         return nothing
     end
-    ovec = re.ovec
-    n = div(length(ovec),2) - 1
-    mat = SubString(str, ovec[1]+1, prevind(str, ovec[2]+1))
-    cap = Union{Nothing,SubString{String}}[ovec[2i+1] == PCRE.UNSET ? nothing :
-                                        SubString(str, ovec[2i+1]+1,
-                                                  prevind(str, ovec[2i+2]+1)) for i=1:n]
-    off = Int[ ovec[2i+1]+1 for i=1:n ]
-    RegexMatch(mat, cap, ovec[1]+1, off, re)
+    n = div(Base.PCRE.ovec_length(data), 2) - 1
+    p = Base.PCRE.ovec_ptr(data)
+    mat = SubString(str, unsafe_load(p, 1)+1, prevind(str, unsafe_load(p, 2)+1))
+    cap = Union{Nothing,SubString{String}}[unsafe_load(p,2i+1) == Base.PCRE.UNSET ? nothing :
+                                        SubString(str, unsafe_load(p,2i+1)+1,
+                                                  prevind(str, unsafe_load(p,2i+2)+1)) for i=1:n]
+    off = Int[ unsafe_load(p,2i+1)+1 for i=1:n ]
+    result = RegexMatch(mat, cap, unsafe_load(p,1)+1, off, re)
+    Base.PCRE.free_match_data(data)
+    return result
 end
 
 pmatch(r::Regex, s::AbstractString) = pmatch(r, s, firstindex(s))
@@ -113,17 +125,15 @@ function checked_ts_expr(name::Expr, ts_expr::Expr)
         shouldrun = length(rs.stack) <= rs.maxdepth &&
                 pmatch(rs.include, path) != nothing && pmatch(rs.exclude, path) == nothing
         rs.seen[path] = shouldrun
-        ts_obj = if shouldrun
+        if shouldrun
             print("Running ")
             printstyled(path; bold=true)
             println(" tests...")
             $ts_expr
         else
             printstyled("Skipping $path tests...\n"; color=:light_black)
-            nothing
         end
         close_testset(rs)
-        ts_obj
     end
 end
 
