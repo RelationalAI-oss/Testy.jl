b1b57795e5d52035c17f8bef0de4f3a492c7bde9
Nathan Daly <nathan.daly@relational.ai>
2019-10-17 13:07:25 -0400

    Create Julia VERSION checks to make Testy's regex lib work for julia v1.2 and v1.3

--- src/Testy.jl
+++ src/Testy.jl
@@ -3,19 +3,27 @@ module Testy
 using Test
 
 # TODO refactor into patch to bring these into Base
-"""
-Helper for `pmatch`.
-"""
+
+# NOTE: Since  the internals have changed in julia v1.3.0, we need VERSION checks inside
+# these functions.
+
+# Helper for `pmatch`: Mirrors Base.PCRE.exec
 function pexec(re, subject, offset, options, match_data)
     rc = ccall((:pcre2_match_8, Base.PCRE.PCRE_LIB), Cint,
                (Ptr{Cvoid}, Ptr{UInt8}, Csize_t, Csize_t, Cuint, Ptr{Cvoid}, Ptr{Cvoid}),
-               re, subject, sizeof(subject), offset, options, match_data, Base.PCRE.get_local_match_context())
+               re, subject, sizeof(subject), offset, options, match_data,
+               @static if VERSION >= v"1.3-"
+                   Base.PCRE.get_local_match_context()
+               else
+                   Base.PCRE.MATCH_CONTEXT[]
+               end)
     # rc == -1 means no match, -2 means partial match.
     rc < -2 && error("PCRE.exec error: $(err_message(rc))")
     rc
 end
 
-# Mirrors Base.PCRE.exec_r_data: helper to call pexec w/ thread-safe match data
+# Helper to call `pexec` w/ thread-safe match data: Mirrors Base.PCRE.exec_r_data
+# Only used for Julia Version >= v"1.3-"
 function pexec_r_data(re, subject, offset, options)
     match_data = Base.PCRE.create_match_data(re)
     ans = pexec(re, subject, offset, options, match_data)
@@ -31,22 +39,38 @@ is set in `re.match_options`).
 function pmatch(re::Regex, str::Union{SubString{String}, String}, idx::Integer, add_opts::UInt32=UInt32(0))
     Base.compile(re)
     opts = re.match_options | add_opts
-    # rc == -1 means no match, -2 means partial match.
-    rc, data = pexec_r_data(re.regex, str, idx-1, opts)
-    if rc == -1 || (rc == -2 && (re.match_options & Base.PCRE.PARTIAL_HARD) == 0)
+    @static if VERSION >= v"1.3-"
+        # rc == -1 means no match, -2 means partial match.
+        rc, data = pexec_r_data(re.regex, str, idx-1, opts)
+        if rc == -1 || (rc == -2 && (re.match_options & Base.PCRE.PARTIAL_HARD) == 0)
+            Base.PCRE.free_match_data(data)
+            return nothing
+        end
+        n = div(Base.PCRE.ovec_length(data), 2) - 1
+        p = Base.PCRE.ovec_ptr(data)
+        mat = SubString(str, unsafe_load(p, 1)+1, prevind(str, unsafe_load(p, 2)+1))
+        cap = Union{Nothing,SubString{String}}[unsafe_load(p,2i+1) == Base.PCRE.UNSET ? nothing :
+                                            SubString(str, unsafe_load(p,2i+1)+1,
+                                                      prevind(str, unsafe_load(p,2i+2)+1)) for i=1:n]
+        off = Int[ unsafe_load(p,2i+1)+1 for i=1:n ]
+        result = RegexMatch(mat, cap, unsafe_load(p,1)+1, off, re)
         Base.PCRE.free_match_data(data)
-        return nothing
+        return result
+    else  #  Julia VERSION < v"1.3"
+        # rc == -1 means no match, -2 means partial match.
+        rc = pexec(re.regex, str, idx-1, opts, re.match_data)
+        if rc == -1 || (rc == -2 && (re.match_options & Base.PCRE.PARTIAL_HARD) == 0)
+            return nothing
+        end
+        ovec = re.ovec
+        n = div(length(ovec),2) - 1
+        mat = SubString(str, ovec[1]+1, prevind(str, ovec[2]+1))
+        cap = Union{Nothing,SubString{String}}[ovec[2i+1] == PCRE.UNSET ? nothing :
+                                            SubString(str, ovec[2i+1]+1,
+                                                      prevind(str, ovec[2i+2]+1)) for i=1:n]
+        off = Int[ ovec[2i+1]+1 for i=1:n ]
+        RegexMatch(mat, cap, ovec[1]+1, off, re)
     end
-    n = div(Base.PCRE.ovec_length(data), 2) - 1
-    p = Base.PCRE.ovec_ptr(data)
-    mat = SubString(str, unsafe_load(p, 1)+1, prevind(str, unsafe_load(p, 2)+1))
-    cap = Union{Nothing,SubString{String}}[unsafe_load(p,2i+1) == Base.PCRE.UNSET ? nothing :
-                                        SubString(str, unsafe_load(p,2i+1)+1,
-                                                  prevind(str, unsafe_load(p,2i+2)+1)) for i=1:n]
-    off = Int[ unsafe_load(p,2i+1)+1 for i=1:n ]
-    result = RegexMatch(mat, cap, unsafe_load(p,1)+1, off, re)
-    Base.PCRE.free_match_data(data)
-    return result
 end
 
 pmatch(r::Regex, s::AbstractString) = pmatch(r, s, firstindex(s))
